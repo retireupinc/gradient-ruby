c032cf5f6e08feac8d0fb0317773d7051003baec
David Stuckmann <david@retireup.com>
2018-12-07 17:11:56 -0600

    Update contact attributes

--- lib/gradient_ruby/models/contact.rb
+++ lib/gradient_ruby/models/contact.rb
@@ -3,31 +3,23 @@ module GradientRuby
     class Contact < APIModel
       attribute :id, String
       attribute :name, String
-      attribute :title, String
+      attribute :prefix, String
       attribute :first_name, String
       attribute :middle_name, String
       attribute :last_name, String
       attribute :suffix, String
-      attribute :status, String
       attribute :gender, String
       attribute :date_of_birth, DateTime
-      attribute :social_security_number, String
-      attribute :greeting, String
       attribute :drivers_license_number, String
       attribute :drivers_license_state, String
       attribute :drivers_license_expiration_date, DateTime
       attribute :is_citizen, Boolean
-      attribute :age, Integer
       attribute :marital_status, String
-      attribute :email_addresses, Array
-      attribute :phone_numbers, Array
-      attribute :physical_addresses, Array
-      attribute :retirement, Hash
-      attribute :social_security, Hash
-      attribute :financial_priorities, Hash
+      attribute :phone_number, String
+      attribute :physical_address, String
 
-      json_attributes :name, :title, :first_name, :middle_name, :last_name, :suffix,
-                      :email_addresses, :date_of_birth, :gender, :marital_status
+      json_attributes :name, :prefix, :first_name, :middle_name, :last_name, :suffix,
+                      :date_of_birth, :gender, :marital_status
 
       def save
         if @id
