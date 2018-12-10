7184b526749824aca1c393452b2034cc05e6f425
David Stuckmann <david@retireup.com>
2018-12-10 14:08:44 -0600

    Fix module name

--- lib/gradient_ruby/models/spouse.rb
+++ lib/gradient_ruby/models/spouse.rb
@@ -1,21 +1,21 @@
-module WealthboxRuby
+module GradientRuby
   module Models
     class Spouse < Base
-        attribute :id, String
-        attribute :name, String
-        attribute :prefix, String
-        attribute :first_name, String
-        attribute :middle_name, String
-        attribute :last_name, String
-        attribute :suffix, String
-        attribute :gender, String
-        attribute :date_of_birth, DateTime
-        attribute :drivers_license_number, String
-        attribute :drivers_license_state, String
-        attribute :drivers_license_expiration_date, DateTime
-        attribute :is_citizen, Boolean
-        attribute :marital_status, String
-        attribute :phone_number, String
+      attribute :id, String
+      attribute :name, String
+      attribute :prefix, String
+      attribute :first_name, String
+      attribute :middle_name, String
+      attribute :last_name, String
+      attribute :suffix, String
+      attribute :gender, String
+      attribute :date_of_birth, DateTime
+      attribute :drivers_license_number, String
+      attribute :drivers_license_state, String
+      attribute :drivers_license_expiration_date, DateTime
+      attribute :is_citizen, Boolean
+      attribute :marital_status, String
+      attribute :phone_number, String
     end
   end
 end
